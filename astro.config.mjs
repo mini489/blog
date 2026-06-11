3d323bcbd6a53e85b1241e6f3aa8104345c1b4ae
Wataru Yamazaki <wataruyamazaki@MacBookAir.home-life.hub>
2026-06-11 02:01:20 +0200

    change sitemap url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://rhan4.com',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
