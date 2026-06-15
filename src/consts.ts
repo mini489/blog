a7cecc8ee37f92a8f4799e5d7f7900f002ed19c2
Wataru Yamazaki <wataruyamazaki@pasokon.local>
2026-06-15 22:15:34 +0200

    画像削除、ブログ名とヘッダー変更、ホーム編集

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'RHANのブログ';
-export const SITE_DESCRIPTION = 'RHANの思考と記録を残すブログです';
+export const SITE_TITLE = 'RHANの書置部屋';
+export const SITE_DESCRIPTION = 'RHANが思ったことを、誰に宛てるでもなく書いているページです';
